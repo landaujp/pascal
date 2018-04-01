392992eadbc9203cb3170d47a08d936699b92c0d
ishino <shungo.ishino@gmail.com>
2018-04-01 14:50:07 +0900

    rydbergからdocekrのファイルをコピー。
    少しいじったが、まだ動かない。

---
+++ docker/mysql/initial.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `messages` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `message` varchar(40) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
